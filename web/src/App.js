c0e42196d3e80b60d059f8aac477232dd8495224
Adam Siemer <14990007+abcmer@users.noreply.github.com>
2020-06-09 19:59:20 -0500

    update UI to fetch hotdog joints from server and list them

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [joints, setJoints] = useState([])
+  const fetchJointsData = async () => {
+    const response = await axios.get(`http://localhost:5000/api/joints`)
+    setJoints(response.data)
+  }
+
+  useEffect(() => {
+    fetchJointsData()
+  }, [])
+
+  return(
+    <div>
+      {joints.map(j => <h1 key={j._id}>{j.name}</h1>)}
     </div>
-  );
+  )
 }
 
 export default App;
