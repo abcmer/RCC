b309bab2ee6b91cb074daaa7ef723b80107a3425
Adam Siemer <14990007+abcmer@users.noreply.github.com>
2020-06-27 10:39:11 -0500

    add Login google auth to top nav bar.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import MovieList from './MovieList/MovieList'
 import TopNav from './TopNav/TopNav'
 import './App.css';
 
+
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [user, setUser] = useState({})
+
+  console.log('user', user)
   return(
     <div className='App'>
-      <TopNav appName="Red Carpet Quest"/>
+      <TopNav 
+        appName="Red Carpet Quest" 
+        isAuthenticated={isAuthenticated}
+        user={user}
+        setUser={setUser}
+      />
       <MovieList/>
     </div>
     )
--- client/src/TopNav/TopNav.js
+++ client/src/TopNav/TopNav.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { GoogleLogin } from 'react-google-login';
+
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+
 
 import './TopNav.css'
 
@@ -28,9 +31,23 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function TopNav(props) {
-  const {appName} = props;
+  const {appName, user, setUser} = props;
   const classes = useStyles();
+  let loginText
+  if (user) {
+    loginText = 'LOGOUT'
+  } else {
+    loginText = 'LOGIN'
+  }
 
+  const responseGoogle = (response) => {
+    const user = {
+      ...response.profileObj,
+      ...response.tokenObj
+    }
+    console.log(user)
+    setUser(user)
+  }
   return (
     <div className={classes.root}>
       <AppBar position="static" className={classes.appBar}>
@@ -41,7 +58,24 @@ export default function TopNav(props) {
           <Typography variant="h6" className={classes.title}>
             {appName}
           </Typography>
-          <Button color="inherit">Login</Button>
+          {!user? (
+            <GoogleLogin
+              clientId="949347339047-8s7hh1v5k9m32g857piefi0omolkr6t6.apps.googleusercontent.com"
+              buttonText="Logout"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={'single_host_origin'}
+            />
+          ) : (
+            <GoogleLogin
+              clientId="949347339047-8s7hh1v5k9m32g857piefi0omolkr6t6.apps.googleusercontent.com"
+              buttonText="Login"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={'single_host_origin'}
+            />
+          )                 
+          }
         </Toolbar>
       </AppBar>
     </div>
