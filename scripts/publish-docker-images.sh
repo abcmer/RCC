0581950f1cb96bd807d9c77997442fa4522e6513
Adam Siemer <14990007+abcmer@users.noreply.github.com>
2021-01-16 16:10:41 -0600

    add some logging to public-docker-images.sh

--- scripts/publish-docker-images.sh
+++ scripts/publish-docker-images.sh
@@ -1,8 +1,13 @@
+echo 'Building docker images'
 docker-compose build
 export git_commit=$(git rev-parse HEAD)
 
+echo Tagging rcc-server with tag $git_commit
 docker tag rcc-server:latest abcmer/rcc-server:$git_commit
+echo Publishing rcc-server:$git_commit
 docker push abcmer/rcc-server:$git_commit
 
+echo Tagging rcc-client with tag $git_commit
 docker tag rcc-client:latest abcmer/rcc-client:$git_commit
+echo Publishing rcc-client:$git_commit
 docker push abcmer/rcc-client:$git_commit
